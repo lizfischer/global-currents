1717ed610325c2d4b2658077de0d79ec9f11f907
lizfischer <efisch17@stanford.edu>
2016-04-07 14:55:25 -0700

    fixing use of siblings

--- search/temp/main.js
+++ search/temp/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.result-wrap').click(function() {
+    $('.result-wrap img').click(function() {
         $(this).siblings('.result-data').fadeIn(300).delay(1000).fadeOut(300);
     });
 });
